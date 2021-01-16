8bb28d5bc3aa5075723f0e26c9d37a252116411c
ajisuryawan <kazumovie132@gmail.com>
2021-01-16 09:41:16 +0700

    Initial commit

---
+++ app/src/main/java/com/apps/recyclerview/MainMenu.java
@@ -0,0 +1,26 @@
+package com.apps.recyclerview;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+
+        CardView menu1= (CardView)findViewById(R.id.menu1);
+        menu1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            }
+        });
+
+    }
+}
